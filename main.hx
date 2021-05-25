e6d306c40f17047e647f98e600014a75ebbdcc9f
RagDev <71398486+TheOriginalRagdev@users.noreply.github.com>
2021-05-24 23:42:00 -0400

    Create main.hx

---
+++ main.hx
@@ -0,0 +1,12 @@
+import js.Browser;
+class Main {
+    static function main() {
+        var button = Browser.document.createButtonElement();
+        button.textContent = "Click me!";
+        button.onclick = (event) -> Browser.alert("Haxe is great");
+        Browser.document.body.appendChild(button);
+    }
+}
+haxe --js js/app.js --main Main
+  --js js/app.js
+--main Main
